c05a27af94ee13355a86e6afbc16bb12c264af10
José Guilherme <105402150+guilhermeverissimo14@users.noreply.github.com>
2026-08-02 18:16:13 -0300

    feat: add entity Product

---
+++ products/src/main/java/shopping/ecommerce/products/model/Product.java
@@ -0,0 +1,28 @@
+package shopping.ecommerce.products.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "products")
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long code;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "unit_price", nullable = false, precision = 16, scale = 2)
+    private BigDecimal unitPrice;
+
+}
